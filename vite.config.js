4f04f66cdf1d8967c025f1799c33af19a7d5c26d
Viktor <rudvamailbox@gmail.com>
2022-11-02 22:25:10 +0200

    init

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    server: {
+        port: 5173,
+        host: '0.0.0.0',
+        hmr: {
+            host: 'usa.localhost'
+        },
+    },
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+});
