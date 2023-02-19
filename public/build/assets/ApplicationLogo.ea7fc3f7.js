468d150754506e846b0ff7da917bce119966d1ee
user <rvainc>
2023-02-19 20:43:07 +0200

    build

---
+++ public/build/assets/ApplicationLogo.ea7fc3f7.js
@@ -0,0 +1,1 @@
+import{_ as c}from"./_plugin-vue_export-helper.cdc0426e.js";const n={};function o(r,t){return null}const e=c(n,[["render",o]]);export{e as A};
---
+++ public/build/assets/helpers.ba09182c.js
@@ -0,0 +1,1 @@
+import{m as o}from"./AuthenticatedLayout.af65744a.js";const a=m=>o(m).format("DD.MM.YYYY");export{a as f};
