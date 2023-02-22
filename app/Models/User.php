3c599f26cd580d1435af1f5c32c06de7b45ff32f
user <rvainc>
2023-02-22 20:06:11 +0200

    email verification enabled

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,7 @@ use Laravel\Sanctum\HasApiTokens;
  *
  * @method static static|Builder query()
  */
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use
         HasApiTokens,
